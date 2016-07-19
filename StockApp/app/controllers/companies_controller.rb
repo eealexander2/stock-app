62d553a70013a428446c00cfb75adb3b72485034
Elizabeth Alexander <eealexander2@gmail.com>
2016-07-19 14:15:59 -0500

    Adds-first-draft

--- StockApp/app/controllers/companies_controller.rb
+++ StockApp/app/controllers/companies_controller.rb
@@ -8,17 +8,19 @@ class CompaniesController < ApplicationController
 	def index
   	@companies = Company.all
   	if params[:search]
-    	@companies = Company.search(params[:search]).order('name ASC')
+  		prefix = params[:search][0..3]
+    	@companies = Company.search(prefix).order('name ASC')
   	else
     	@companies = Company.all.order('name ASC')
   	end
 	end
 
 	def show
-		stock = StockQuote::Stock.history("VZ", (Date.today - 42), Date.today)
+		company = Company.find(params[:id])
+		stock = StockQuote::Stock.history( company.symbol, (Date.today - 42), Date.today)
 		@plotting_points = []
 		stock.each do |share|
-			y_value = ((share.open) + (share.low) + (share.close) + (share.high)/4) 
+			y_value = (((share.open) + (share.low) + (share.close) + (share.high))/4) 
 			@plotting_points << [share.date, y_value]
 		end
 			
@@ -34,7 +36,7 @@ class CompaniesController < ApplicationController
 		
 		@labels = labels.reverse.to_json
 		@data = data.reverse.to_json 
-	
+		@company = company.name 
 	end 
 
 end 
--- StockApp/app/models/company.rb
+++ StockApp/app/models/company.rb
@@ -2,8 +2,11 @@ require 'csv'
 
 class Company < ActiveRecord::Base
 
-	def self.search(search)
-	  where("name ILIKE ?", "%#{search}%") || where("symbol ILIKE ?", "%#{search}%")
+	def self.search(prefix)
+		@found_companies = []
+	  @found_companies << Company.where("name ILIKE ?", "%#{prefix}%") 
+	  @found_companies << Company.where("symbol ILIKE ?", "%#{prefix}%") 
+	  @found_companies.flatten
 	end
 
 end 
