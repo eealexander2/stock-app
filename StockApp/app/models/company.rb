259b3577f278b9f8ca01a7b3fa52ffc49c76ad4d
Elizabeth Alexander <eealexander2@gmail.com>
2016-07-21 10:07:27 -0500

    Add formatting troubleshooting

--- StockApp/app/models/company.rb
+++ StockApp/app/models/company.rb
@@ -3,33 +3,33 @@ class Company < ActiveRecord::Base
 
 	def self.search(prefix)
 		@found_companies = [] 
-	  @found_companies << where("name ILIKE ?", "#{prefix}%").order('name ASC')
-	  @found_companies << where("symbol ILIKE ?", "#{prefix}%").order('name ASC')
-	end
+    @found_companies << where("name ILIKE ?", "#{prefix}%").order('name ASC')
+    @found_companies << where("symbol ILIKE ?", "#{prefix}%").order('name ASC')
+  end
 
-	def self.graph_data(stock)
-	  data_points = []
-		stock.each do |share|
-			y_value = (((share.open) + (share.low) + (share.close) + (share.high))/4) 
-			data_points << [share.date, y_value]
-		end	
-		data_points
-	end 
+  def self.graph_data(stock)
+    data_points = []
+    stock.each do |share|
+      y_value = (((share.open) + (share.low) + (share.close) + (share.high))/4) 
+      data_points << [share.date, y_value]
+    end	
+    data_points
+  end 
 
-	def self.x_axis(data_points)
-		labels = [] 
-		data_points.each do |point|
-			labels << point[0]
-		end 
-		labels.reverse.to_json	
-	end 
+  def self.x_axis(data_points)
+    labels = [] 
+    data_points.each do |point|
+      labels << point[0]
+    end 
+    labels.reverse.to_json	
+  end 
 
-	def self.y_axis(data_points)
-		data = []
-		data_points.each do |point|
-			data << point[1]		
-		end 	
-		data.reverse.to_json
+  def self.y_axis(data_points)
+  	data = []
+  	data_points.each do |point|
+      data << point[1]		
+    end 	
+    data.reverse.to_json
 	end 
 
 end 
